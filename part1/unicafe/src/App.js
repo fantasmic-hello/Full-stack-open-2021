f955774f03abbc71a39d770d57c7528e9d7c4346
Mic. G <fantasmic-hello@users.noreply.github.com>
2021-08-05 13:48:48 +0200

    Add unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,30 +1,57 @@
 import React, {useState} from "react";
 
-const Display = (props) => {
-    return(
-      <div>{props.text} {props.value}</div>
-    )
+const Display = (props) =>  <div>{props.text} {props.value}</div> 
+ 
+
+const Statistics = ({good, neutral, bad, total}) => {
+  console.log(good);
+  return (
+    
+    <div> Average{(good - bad)/total}</div>
+  )
+  
 }
+
+
 
 const App = () => {
 
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  //const [average, setAverage] = useState(0);
+  //const [percent, setPercent] = useState(0);
 
   const handleGood = () =>{
     console.log('Hello good');
     setGood(good + 1);
+    setTotal(total + 1);
+    
   }
   const handleNeutral = () => {
     setNeutral(neutral + 1);
     console.log('Hello neutral');
+    setTotal(total + 1);
+    
   }
   const handleBad = () => {
     setBad(bad + 1);
     console.log('Hello bad');
-
+    setTotal(total + 1);
+   
   }
+
+ /* const handleAverage = () => {
+    setAverage( (good * 1) + (neutral * 0) + (bad * -1) / allClicks   );
+   return average;
+  }
+
+  const handlePercent = () => {
+    setPercent( (good / allClicks) * 100 )
+    return percent;
+  }*/
+
  
   return (
     <div>
@@ -39,6 +66,8 @@ const App = () => {
         <Display value={good} text = "Good"/>
         <Display value={neutral} text = "Neutral" />
         <Display value={bad} text = "Bad" />
+        <Display value={total} text = "All" />
+        <Statistics good={good} neutral={neutral} bad={bad} total={total}  />
       </div>  
     </div>
 
