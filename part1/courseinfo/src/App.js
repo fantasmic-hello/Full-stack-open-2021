45d0f58116e38c9ddb02d7715a18afc48bb7cf05
Mic. G <fantasmic-hello@users.noreply.github.com>
2021-08-06 18:30:45 +0200

    Add exercise 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,11 +23,9 @@ const Part = (props) =>{
 const Content = (props) =>{
     return(
     <div>
-        
             <Part part={props.part[0].name} exercise={props.part[0].exercises}/>
             <Part part={props.part[1].name} exercise={props.part[1].exercises}/>
             <Part part={props.part[2].name} exercise={props.part[2].exercises}/>
-       
     </div>    
     )
 }
