46da242f36d7ebd11989095ff72b6bc982f58285
Mic. G <fantasmic-hello@users.noreply.github.com>
2021-11-18 13:05:05 +0100

    part2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,37 @@
-
-import './App.css';
+import React, {useState} from "react";
 
 const App = () => {
+ 
+ const [persons, setPerson] = useState(
+    [
+     {name: 'Kalle' }
+    ])
+  const [newName, setNewName] = useState('')
+ 
+  const addName = (event) => {
+    event.preventDefault()
+    console.log("Event target", event.target)
+  }
+
+  const nameChange = (event) =>{
+    
+    console.log("Event target", event.target.value)
+    setNewName(event.target.value)
+  }
+  
+ 
   return (
     <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={addName}>
+          name: <input 
+          value={newName} 
+          onChange={nameChange} />
+       
+          <button type='submit'>add</button>
+        
+      </form>
+      <h2>Numbers</h2>
     </div>
   );
 }
