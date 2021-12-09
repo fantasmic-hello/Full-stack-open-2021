801396f7fb728b0aa1c0266cda669f8322777245
Mic. G <fantasmic-hello@users.noreply.github.com>
2021-12-09 20:44:19 +0100

    better useEffect in step2

--- part2/data_for_countries/src/components/Country.js
+++ part2/data_for_countries/src/components/Country.js
@@ -1,57 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FullCountry from "./CountryInfo";
 
-const DisplayCountry = ({ countries }) => {
-  const [searchTerm, setSearchTerm] = useState("");
 
-  const countriesToShow = countries.filter((country) =>
-    country.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
-  const buttonClick = (country) => {
-    setSearchTerm(country.name);
 
-  };
 
-  if (countriesToShow.length > 10) {
-    return (
-      <div>
-        <label>find countries</label>
-        <input
-          type="text"
-          onChange={(event) => setSearchTerm(event.target.value)}
-        ></input>
-        {searchTerm == "" ? "" : "Too many countries"}
-      </div>
-    );
-  } else if (countriesToShow.length === 1) {
-    return (
-      <div>
-        <label>find countries</label>
-        <input
-          type="text"
-          onChange={(event) => setSearchTerm(event.target.value)}
-        ></input>
-        <FullCountry country={countriesToShow[0]}></FullCountry>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <label>find countries</label>
-        <input
-          type="text"
-          onChange={(event) => setSearchTerm(event.target.value)}
-        ></input>
-        {countriesToShow.map((country) => (
-          <div>
+const DisplayCountry = ({countries, searchTerm}) =>{
+
+    const[countryList, setCountryList] = useState(countries);
+
+
+
+    useEffect(()=>{
+        setCountryList(countries.filter(
+            country => country.name.toLowerCase().includes(searchTerm.toLowerCase())
+        ))
+    
+    },[searchTerm])
+    
+
+    const showSingleCountry = (c) =>{
+        setCountryList(countries.filter(country => country.name.toLowerCase() === c.name.toLowerCase()))
+    }
+
+    
+    if(countryList.length > 10){
+     
+        return (
+            <div>
+             {searchTerm == "" ? "" : 'Too many countries'}
+             </div>
+             );
+
+    } else if(countryList.length === 1){
+        
+        return ( <div>
+            <FullCountry country={countryList[0]}></FullCountry>
+        </div>)
+    } 
+    
+    else {
+    
+    return(
+        <div>
+            {countryList.map(country => 
+            <div>
             <p key={country.numericCode}>{country.name}</p>
-            <button onClick={() => buttonClick(country)}>show</button>
-          </div>
-        ))}
-      </div>
-    );
-  }
-};
+            <button onClick={() => showSingleCountry(country)}>show</button>
+            </div>
+            )}
+        </div>
+    )
+}
+        
+}
 
 export default DisplayCountry;
--- part2/data_for_countries/src/components/CountryInfo.js
+++ part2/data_for_countries/src/components/CountryInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FullCountry = ({country}) =>{
     
-    console.log("Full", country)
+
    
     return(
     <div>
